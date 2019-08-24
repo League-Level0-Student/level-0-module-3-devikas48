9606743497c6ca5cdb2ce1ce322dee4cace16cb1
League Student <league@iMac-12.attlocal.net>
2019-08-24 11:29:05 -0700

    makeup day

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -11,9 +11,26 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.show();
+		rob.penDown();
+		rob.setSpeed(100);
+        rob.setX(50);
 		// 2. Make the robot draw a star shape. Hint: 144.
+   rob.turn(90);
 
+   for (int i = 0; i < 5; i++) {
+	
+rob.move(30);
+   for (int j = 0; j < 4; j++) {
+	 
+	   rob.turn(144);
+	     rob.move(30);
+}
+   rob.penUp();
+   rob.setAngle(90);
+   rob.move(100);
+   rob.penDown();
+   }
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
---
+++ src/yearalive.java
@@ -0,0 +1,22 @@
+
+public class yearalive {
+public static void main(String[] args) {
+	
+   for (int i = 2004; i < 2020; i++) {
+	System.out.println(i);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
